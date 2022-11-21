f645c70cd93ceee1205be0c4d2107b927d30c525
Adrianb18 <118032411+Adrianb18@users.noreply.github.com>
2022-11-20 21:56:38 -0500

    Seperate codes (for red change 7 in yellow to 8 or 6 in green to 8(red error))

---
+++ Programs/Yellow LED/Yellow LED.ino
@@ -0,0 +1,20 @@
+//Initializing LED Pin
+int led_pin = 8;
+
+void setup() {
+  //Declaring LED pin as output
+  pinMode(led_pin, OUTPUT);
+}
+
+void loop() {
+  //Fading the LED
+  for(int i=0; i<255; i++){
+    analogWrite(led_pin, i);
+    delay(5);
+  }
+
+  for(int i=255; i>0; i--){
+    analogWrite(led_pin, i);
+    delay(5);
+  }
+}
