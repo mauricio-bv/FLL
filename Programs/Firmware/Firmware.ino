573bbb817301b261f9a44958dbeb016f5e46b8e5
Adrianb18 <118032411+Adrianb18@users.noreply.github.com>
2022-11-23 23:39:17 -0500

    Implemented First functional program for  Battery management

--- Programs/Firmware/Firmware.ino
+++ Programs/Firmware/Firmware.ino
@@ -1,13 +1,44 @@
-
+float AH;
+bool critical_message_send = false;
+bool warning_message_send = false;
 
 void setup() {
-  Serial.begin(9600);
-  Serial.println("Innitializing system...");
   initializeLEDs();
+  InitAccel();
 }
 
 void loop() {
-  charging();
-  readVoltage();
-  delay(200);
+  AH = GetAH();
+  Serial.print("AH = ");
+  Serial.println(AH);
+  ReadAccelVals();
+  AddValues(GetAccelX(), GetAccelY());
+
+  if (IsMoving()) {
+    SwitchAllLEDs();
+  } else {
+    //SequenceLEDs();
+    turnLEDsOff();
+    if (AH <= 70) {
+      breathGreenLED();
+      if (critical_message_send) {
+        warning_message_send = false;
+        critical_message_send = false;
+      }
+    } else if (AH > 90) {
+      BlinkRedLED();
+      if (!critical_message_send) {
+        //sendCriticalMessage();
+        critical_message_send = true;
+      }
+    } else {
+      BlinkYellowLED();
+
+      if (!warning_message_send) {
+        //sendWarningMessage();
+        warning_message_send = true;
+      }
+    }
+  }
+  delay(100);
 }
